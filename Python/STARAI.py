068fca5954dca5b427743bf041b873a52a8920fe
GLiTcH-point <jacquesdnoel@gmail.com>
2025-10-05 10:30:21 +0200

    Updates

--- Python/STARAI.py
+++ Python/STARAI.py
@@ -1,119 +1,16 @@
-import os
 import joblib
-import random
 import numpy as np
-import pandas as pd
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import TensorDataset, DataLoader
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.utils.class_weight import compute_class_weight
-from sklearn.experimental import enable_iterative_imputer
-from sklearn.impute import IterativeImputer
-from rich import print
-
-os.chdir('C:/Coding/PythonScripts/Data')
-
-#-----------------------------------------------------------------------------------------------------------------------
-#Data import
-
-columnnames = ['Confirmation','OrbitalPeriod','PlanetRadius','InsolationFlux','EquilibriumTemp','StellarEffectiveTemp','StellarRadius','RA','Dec']
-
-filepath = 'ExoPlanetHarmony.csv' #ex: KOI/TOI/K2 cumulative table data csv file path
-
-'''This code can be used to concactenate more databases
-fileKOI = 'KOIHarmony.csv'
-fileK2 = 'K2Harmony.csv'
-fileTOI = 'TOIHarmony.csv'
-
-dfk = pd.read_csv(fileKOI, skiprows=30,usecols=[0,1,4,7,10,13,19,22,23]) #f1=0.79
-dfk.columns = columnnames
-dfk2 = pd.read_csv(fileK2, skiprows=17,usecols=[0,1,2,3,4,5,6,8,10]) #bad ratio
-dfk2.columns = columnnames
-dft = pd.read_csv(fileTOI, skiprows=35,usecols=[0,1,5,9,13,17,21,26,28]) #0.57
-dft.columns = columnnames
-'''
+import torch.nn.functional as F
 
 
+#pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu126 #pour installer pytorch avec cuda (gpu) si besoin
 
-binary_replace = {'CANDIDATE':'True', 
-                  'FALSE POSITIVE': 'False', 
-                  'NOT DISPOSITIONED': 'False', 
-                  'CONFIRMED': 'True',
-                  'REFUTED': 'False',
-                  'APC': 'False',
-                  'CP': 'True',
-                  'FP': 'False',
-                  'FA': 'False',
-                  'KP': 'True',
-                  'PC': 'True'}
+#Loading scalers used for AI training
 
-
-df = pd.read_csv(filepath)
-
-df = df.applymap(lambda x: binary_replace.get(x, x) if isinstance(x, str) else x)
-
-#-----------------------------------------------------------------------------------------------------------------------
-#Data set-up
-
-
-features = ['OrbitalPeriod','PlanetRadius','InsolationFlux','EquilibriumTemp','StellarEffectiveTemp','StellarRadius','RA','Dec']   # replace with your numeric columns that you want to keep
-label_col = 'Confirmation'                   # replace with your target column that you want your model to predict
-
-imputer = IterativeImputer()
-df[features] = imputer.fit_transform(df[features])
-
-# numeric features -> StandardScaler
-scaler = StandardScaler()
-X = scaler.fit_transform(df[features].values.astype(np.float32)) #(value-moyenne)/ecart-type to center data and get rid of unit
-
-# encode text labels to integers (0..K-1)
-le = LabelEncoder()
-Y = le.fit_transform(df[label_col].values)   #transforms labels to integers
-
-joblib.dump(scaler, "scaler.pkl") # save the scaler for later use
-joblib.dump(le, "label_encoder.pkl") # save the label encoder for later use
-
-#-----------------------------------------------------------------------------------------------------------------------
-#Data split
-
-# split data into train (70%), temp (30%)
-X_train, X_temp, Y_train, Y_temp = train_test_split(X, Y, test_size=0.30, random_state=42,stratify=Y) # 70% XYtrain, 30% XYtemp, 42 for reproducibility (imagine a minecraft seed)
-
-
-#stratify=Y to keep same class proportions in each split (ex: if 10% of the data is True, and 90% is False, we want the same proportions in train, val and test)
-
-# split temp into val (15%) and test (15%)
-X_val, X_test, Y_val, Y_test = train_test_split(X_temp, Y_temp, test_size=0.5, random_state=42,stratify=Y_temp) # 15% XYval, 15% XYtest (Cuz 50% of 30% is 15%)
-
-#70% training data, 15% validation data, 15% test data
-
-# convert to tensors
-X_train = torch.tensor(X_train, dtype=torch.float32)
-Y_train = torch.tensor(Y_train, dtype=torch.long)   # long for CrossEntropyLoss
-X_val   = torch.tensor(X_val, dtype=torch.float32)
-Y_val   = torch.tensor(Y_val, dtype=torch.long)
-X_test  = torch.tensor(X_test, dtype=torch.float32)
-Y_test  = torch.tensor(Y_test, dtype=torch.long)
-
-#DataLoaders
-train_loader = DataLoader(TensorDataset(X_train, Y_train), batch_size=64, shuffle=True) #feed data in batches of 64, shuffle to randomize order each epoch
-val_loader   = DataLoader(TensorDataset(X_val, Y_val), batch_size=128, shuffle=False)
-test_loader = DataLoader(TensorDataset(X_test, Y_test), batch_size=128, shuffle=False)
-
-#-----------------------------------------------------------------------------------------------------------------------
-#Model definition
-
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {DEVICE}")
-
-def set_seed(seed=42): #setting seed for reproducibility on all libraries
-    random.seed(seed); np.random.seed(seed); torch.manual_seed(seed)
-    if torch.cuda.is_available(): torch.cuda.manual_seed_all(seed)
-set_seed(67)
+scaler = joblib.load("scaler.pkl") #to scale data the same way
+le = joblib.load("label_encoder.pkl") #to convert predictions back to True and False
 
 class SimpleMLP(nn.Module): #Multi Layer Perceptron subclass of nn.Module
 
@@ -138,104 +35,34 @@ class SimpleMLP(nn.Module): #Multi Layer Perceptron subclass of nn.Module
         return self.net(x)
 
 model = SimpleMLP(
-    input_dim=X.shape[1], #number of features in input data
-    hidden=[128,64],  #size of hidden layers, can be changed
+    input_dim=8, #number of features in input data
+    hidden=[1028,512,256,128,64],  #size of hidden layers, can be changed
     num_classes=len(le.classes_) #number of output classes (ex: exoplanet, false positive, candidate)
-    ).to(DEVICE)
-#-------------------------------------------------------------------------------------------------
-#Weights
+    )
 
-cw = compute_class_weight('balanced', classes=np.unique(Y), y=Y)
-class_weights = torch.tensor(cw, dtype=torch.float32).to(DEVICE)
+model.load_state_dict(torch.load("STAR_AI.pth", map_location=torch.device("cpu"))) #to ensure it works even without cpu
+model.eval()  # important for evaluation
 
-'''
-class_counts = np.bincount(Y)    # numpy counts the occurrences of each class in Y with position indicating the class 
-class_weights = torch.tensor(1.0 / (class_counts + 1e-8), dtype=torch.float32).to(DEVICE) # gives more weight to minority classes to help the model learn them better
-class_weights = class_weights / class_weights.sum() * len(class_weights) # normalize the weights around 1
-'''
+def predict_rows(rows):
 
-#When computing the loss, misclassifying a minority class will incur a higher penalty than misclassifying a majority class
+    """
+    rows: list of lists, each inner list = one sample with 8 features
+    returns: list of predicted labels and conf_score
+    """
 
-criterion = nn.CrossEntropyLoss(weight=class_weights)   #multiplies the loss of each class by the weights defined previously
-optimizer = optim.Adam(model.parameters(), lr=1e-3)  #Adam optimizer for weight adjustment, lr=learning rate
+    X_nu = np.array(rows, dtype=np.float32)
+    X_scaled = scaler.transform(X_nu)
+    X_tensor = torch.tensor(X_scaled, dtype=torch.float32)
 
-#-----------------------------------------------------------------------------------------------------------------------
-#Training functions
+    with torch.no_grad():
+        logits = model(X_tensor) #unprocessed  output data
+        probs = F.softmax(logits,dim=1) #probability convert
+        preds = logits.argmax(dim=1).numpy() #gives max
+        labels = le.inverse_transform(preds) #transforms the data into True or False
+        conf_scores = probs.max(dim=1).values.np()
 
-def train_one_epoch(model, loader, optimizer, criterion):
-    #model: the neural network,loader: DataLoader for training data (ex: train_loader), optimizer: optimization algorithm, criterion: loss function
+    return labels,conf_scores
 
-    model.train() #set model to training mode (enables dropout, etc)
-
-    running_loss = 0.0 #to accumulate loss over the epoch
-
-    for Xb, yb in loader:
-        Xb, yb = Xb.to(DEVICE), yb.to(DEVICE)
-        optimizer.zero_grad() #reset gradients from previous step for gradient descent
-        logits = model(Xb) #passes the batch through the model to get raw output scores (logits), shape: (batch, num_classes)
-        loss = criterion(logits, yb) #compute loss between logits and true labels using CrossEntropyLoss function
-        loss.backward() #backpropagation to compute gradients of loss w.r.t. model parameters
-        optimizer.step() #update model parameters using computed gradients and Adam optimizer
-        running_loss += loss.item() * Xb.size(0) #accumulate loss, scaled by batch size
-    return running_loss / len(loader.dataset) #average loss over the epoch
-
-def evaluate(model, loader, criterion):
-
-    model.eval() #set model to evaluation mode (disables dropout, etc)
-
-    running_loss = 0.0 #to accumulate loss over the evaluation
-
-    preds, trues = [], [] #to store predictions and true labels for accuracy calculation
-
-    with torch.no_grad(): #to avoid computing gradients during evaluation 
-        for Xb, yb in loader:
-            Xb, yb = Xb.to(DEVICE), yb.to(DEVICE) 
-            logits = model(Xb)
-            loss = criterion(logits, yb)
-            running_loss += loss.item() * Xb.size(0) #accumulate loss, scaled by batch size
-            pred = logits.argmax(dim=1).cpu().numpy() #get predicted class by taking argmax of logits (dim=1 means that we take the max across rows)
-            #cpu because numpy can't handle cuda tensors
-            preds.append(pred); trues.append(yb.cpu().numpy()) #store predictions and true labels
-    preds = np.concatenate(preds); trues = np.concatenate(trues) #sqish lists into single arrays
-    acc = accuracy_score(trues, preds) #compute accuracy
-    return running_loss / len(loader.dataset), acc #average loss and accuracy over the evaluation
-
-#-----------------------------------------------------------------------------------------------------------------------
-#Training 
-
-best_val_loss = float("inf")  # start with "infinity" so any real loss will be smaller
-
-for epoch in range(1, 201):   # 30 epochs example
-    # --- Train on all batches in the training set ---
-    train_loss = train_one_epoch(model, train_loader, optimizer, criterion) 
-    
-    # --- Evaluate on validation set (no weight updates) ---
-    val_loss, val_acc = evaluate(model, val_loader, criterion)
-    
-    # --- Check if this is the best model so far ---
-    if val_loss < best_val_loss:
-        best_val_loss = val_loss
-        # Save the model's weights to disk
-        torch.save(model.state_dict(), "STAR_AI.pth")   # checkpoint
-    
-    # --- Print progress for this epoch ---
-    print(f"Epoch {epoch:02d} | train_loss {train_loss:.4f} | val_loss {val_loss:.4f} | val_acc {val_acc:.4f}")
-
-
-# This ensures we use the model that performed best on validation data
-model.load_state_dict(torch.load("STAR_AI.pth", map_location=DEVICE))
-
-model.eval()  #same idea as the other times we called our Datakoaders
-preds, trues = [], []
-with torch.no_grad():
-    for Xb, yb in test_loader:
-        Xb, yb = Xb.to(DEVICE), yb.to(DEVICE)
-        logits = model(Xb)
-        pred = logits.argmax(dim=1).cpu().numpy()
-        preds.append(pred) ; trues.append(yb.cpu().numpy())
-preds = np.concatenate(preds)
-trues = np.concatenate(trues)
-
-from sklearn.metrics import classification_report
-labels = np.unique(trues)
-print(classification_report(trues, preds, labels=labels, target_names=le.inverse_transform(labels)))
+rows = [[0.1, 0.3, 0.5, 0.2, 0.9, 0.7, 0.1, 0.05]]
+labels, confidences = predict_rows(rows)
+print(labels[0], confidences[0])
