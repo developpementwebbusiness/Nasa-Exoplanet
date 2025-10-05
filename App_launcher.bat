aa7cc09a7fb58af4f78be41c446779425c95bf7a
Coolcraft71 <doriandepego@gmail.com>
2025-10-05 11:36:17 +0200

    Bat file V2

--- App_launcher.bat
+++ App_launcher.bat
@@ -1,3 +1,6 @@
-start cmd /k docker compose up --build
-timeout /t 20 >nul
+start /B "docker compose up --build" 
+:waitloop
+timeout /t 10 >nul
+powershell -Command "(Invoke-WebRequest -Uri http://localhost:3000 -UseBasicParsing).StatusCode" >nul 2>nul
+if errorlevel 1 goto waitloop
 start "" http://localhost:3000/
